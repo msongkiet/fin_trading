72ae5c9db5357091369857a3956149db4ee97527
msongkiet <m.songkiet@gmail.com>
2021-09-14 16:33:59 +0700

    Update set_cdc_signal.py
    Add weekly chart scan on Friday.
    Add volume scan

--- set_cdc_signal.py
+++ set_cdc_signal.py
@@ -1,3 +1,4 @@
+from datetime import date
 from yf_price import get_yf_price
 from cdc_indi import cdc_indi
 from cdc_scan import cdc_scan
@@ -12,49 +13,57 @@ token = os.environ.get('token_line_test')
 googleSheetId = os.environ.get('googleSheetId_quote')
 
 worksheetName = ['SET100', 'sSET']
+period = ["3mo"]
+interval = ["1d"]
 
-for i in range(len(worksheetName)):
-    url_google_sheet = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(googleSheetId, worksheetName[i])
-    tickers = pd.read_csv(url_google_sheet)
-    tickers = (tickers['Quote'].values.tolist())
+if date.today().isoweekday() == 5: # 5 is Friday
+    period = ["3mo", "1y"]
+    interval = ["1d", "1wk"]
 
-    msg_golden_cross = ''
-    msg_dead_cross = ''
-    msg_pre_buy = ''
-    msg_pre_sell = ''
-    df_vol = pd.DataFrame()
+for k in range(len(period)):
+    for i in range(len(worksheetName)):
+        url_google_sheet = 'https://docs.google.com/spreadsheets/d/{0}/gviz/tq?tqx=out:csv&sheet={1}'.format(googleSheetId, worksheetName[i])
+        tickers = pd.read_csv(url_google_sheet)
+        tickers = (tickers['Quote'].values.tolist())
 
-    for j in range(len(tickers)):
-        try:
-            df_price = get_yf_price(tickers[j])
-            df_price = cdc_indi(df_price)
-            df_price = vol_indi(df_price)
-            df_vol = df_vol.append(df_price.tail(1),ignore_index=False)
+        msg_golden_cross = ''
+        msg_dead_cross = ''
+        msg_pre_buy = ''
+        msg_pre_sell = ''
+        df_vol = pd.DataFrame()
+        
+        for j in range(len(tickers)):
+            try:
+                df_price = get_yf_price(tickers[j], period[k], interval[k])
+                df_price = cdc_indi(df_price)
+                df_price = vol_indi(df_price)
+                print(f'{tickers[j]} {df_price.shape}')
+                df_vol = df_vol.append(df_price.tail(1),ignore_index=False)
 
-            signal = cdc_scan(df_price)
-            if(len(signal)!=0):
-                if(signal == 'bullish'):
-                    msg_golden_cross += f'{tickers[j].split(".")[0]} '
-                elif(signal == 'bearish'):
-                    msg_dead_cross += f'{tickers[j].split(".")[0]} '
-                elif(signal == 'blue'):
-                    msg_pre_buy += f'{tickers[j].split(".")[0]} '
-                elif(signal == 'yellow'):
-                    msg_pre_sell += f'{tickers[j].split(".")[0]} '
-        except Exception as e:
-            print(e)
+                signal = cdc_scan(df_price)
+                if(len(signal)!=0):
+                    if(signal == 'bullish'):
+                        msg_golden_cross += f'{tickers[j].split(".")[0]} '
+                    elif(signal == 'bearish'):
+                        msg_dead_cross += f'{tickers[j].split(".")[0]} '
+                    elif(signal == 'blue'):
+                        msg_pre_buy += f'{tickers[j].split(".")[0]} '
+                    elif(signal == 'yellow'):
+                        msg_pre_sell += f'{tickers[j].split(".")[0]} '
+            except Exception as e:
+                print(e)
 
-    df_vol = df_vol.sort_values(by=['x_of_vol_ma'],ascending=False)   
-    msg_vol_change = ''
+        df_vol = df_vol.sort_values(by=['x_of_vol_ma'],ascending=False)   
+        msg_vol_change = ''
 
-    for row in df_vol.itertuples():
-        if row.x_of_vol_ma >= 1.50:
-            msg_vol_change += f'{row.ticker}:{row.x_of_vol_ma}X, '
-        else:
-            break
+        for row in df_vol.itertuples():
+            if row.x_of_vol_ma >= 1.50:
+                msg_vol_change += f'{row.ticker}:{row.x_of_vol_ma}X, '
+            else:
+                break
 
-    line_notify(f'{worksheetName[i]} CDC Golden Cross: {msg_golden_cross}', token)
-    line_notify(f'{worksheetName[i]} CDC Dead Cross: {msg_dead_cross}', token)
-    line_notify(f'{worksheetName[i]} CDC Pre-Buy Zone: {msg_pre_buy}', token)
-    line_notify(f'{worksheetName[i]} CDC Pre-Sell Zone: {msg_pre_sell}', token)
-    line_notify(f'{worksheetName[i]} Vol change (x of vol ma): {msg_vol_change}', token)
+        line_notify(f'{worksheetName[i]} TF:{interval[k]} CDC Golden Cross: {msg_golden_cross}', token)
+        line_notify(f'{worksheetName[i]} TF:{interval[k]} CDC Dead Cross: {msg_dead_cross}', token)
+        line_notify(f'{worksheetName[i]} TF:{interval[k]} CDC Pre-Buy Zone: {msg_pre_buy}', token)
+        line_notify(f'{worksheetName[i]} TF:{interval[k]} CDC Pre-Sell Zone: {msg_pre_sell}', token)
+        line_notify(f'{worksheetName[i]} TF:{interval[k]} Vol change (x of vol ma): {msg_vol_change}', token)
